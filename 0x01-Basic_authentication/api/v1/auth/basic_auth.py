09315a58a112e0f10adcdf7e3b19aad564502b0d
BeygonK <kipkoechbeygon@gmail.com>
2024-09-10 12:48:38 +0300

    extract credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -33,3 +33,15 @@ class BasicAuth(Auth):
             return base64.b64decode(base64_authorization_header).decode('utf-8')
         except ValueError:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str)-> (str, str):
+        """Returns username and password from decoded Base64 encoded authorization header"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        parts = decoded_base64_authorization_header.split(':')
+        if len(parts) != 2:
+            return None, None
+        return parts[0], parts[1]
