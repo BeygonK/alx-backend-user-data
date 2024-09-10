9f4142b45b99a47828404cebbdf332bc25912b3e
BeygonK <kipkoechbeygon@gmail.com>
2024-09-10 11:43:10 +0300

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,12 @@ class BasicAuth(Auth):
     """Inherits from Auth class
     and implements basic authentication
     """
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Returns Base64 encoded authorization header"""
+        if authorization_header is None or not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        parts = authorization_header.split(' ')
+        return parts[1]
