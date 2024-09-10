57cd237534f26eb26238a973ce01f114468e96ee
BeygonK <kipkoechbeygon@gmail.com>
2024-09-10 11:27:34 +0300

    created basic auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+"""This module contains a class
+that inherits from Auth class
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Inherits from Auth class
+    and implements basic authentication
+    """
