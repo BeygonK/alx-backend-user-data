7599f7a3a8978fb2fdb92678d3f2831affcdb4ec
BeygonK <kipkoechbeygon@gmail.com>
2024-09-10 12:41:00 +0300

    decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """This module contains a class
 that inherits from Auth class
 """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -20,3 +21,13 @@ class BasicAuth(Auth):
             return None
         parts = authorization_header.split(' ')
         return parts[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        """Decodes Base64 encoded authorization header"""
+        if base64_authorization_header is None:
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header).decode('utf-8')
+        except ValueError:
+            return None
