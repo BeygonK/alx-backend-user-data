6212f0546a80f6baa0bd77471946186cc53b37d0
BeygonK <kipkoechbeygon@gmail.com>
2024-09-12 09:45:03 +0300

    created a session class

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -21,6 +21,9 @@ if AUTH_TYPE == 'auth':
 if AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+if AUTH_TYPE == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 excluded = ['/api/v1/status/',
             '/api/v1/unauthorized/',
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module has a class that implements session Authentication"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Inherits from Auth class
+    and implements session authentication
+    """
+    pass
