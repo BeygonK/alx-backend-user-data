e5fb286b9889e15a38c5d64121e8e8eecca78dd7
BeygonK <kipkoechbeygon@gmail.com>
2024-09-12 10:15:10 +0300

    Created a session class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """This module has a class that implements session Authentication"""
+import uuid
 from api.v1.auth.auth import Auth
 
 
@@ -7,4 +8,14 @@ class SessionAuth(Auth):
     """Inherits from Auth class
     and implements session authentication
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str) -> str:
+        """Creates a session id for user id"""
+        if user_id is None:
+            return None
+        if user_id is not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
